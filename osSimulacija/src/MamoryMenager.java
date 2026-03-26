56a57c23585df6540f2cb530fea3b369c1705843
MilanD20 <milanderajic1707@gmail.com>
2026-03-26 22:43:36 +0100

    Dovadanje klase RAM

--- osSimulacija/src/MamoryMenager.java
+++ osSimulacija/src/MamoryMenager.java
@@ -1,12 +1,7 @@
 import java.util.List;
 import java.util.TreeMap;
 
-public class MamoryMenager {
+public class MemoryMenager {
 
-    private int totalSize;
-    private TreeMap<Integer, List<Integer>> freeBlocks;
-    public MamoryMenager(int size) {
-        this.totalSize = size;
-    }
 
 }
