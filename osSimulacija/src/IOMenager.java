c24caa411ed2e078beb0b639cc2023bc6aa25c3f
MilanD20 <milanderajic1707@gmail.com>
2026-03-26 23:24:19 +0100

    Male izmjene u kodu.

--- osSimulacija/src/IOMenager.java
+++ osSimulacija/src/IOMenager.java
@@ -1,17 +1,5 @@
 import java.util.*;
 
 public class IOMenager {
-    private Map<String, IODevice> devices=new HashMap<>();
-
-
-
-    public void requestIO(String deviceName,IODevice device) {
-        IODevice dev = devices.get(deviceName);
-        if( dev == null ) {
-            dev.startOperation(deviceName);
-
-        }
-
-
-    }
+   
 }
