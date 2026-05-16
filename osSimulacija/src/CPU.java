bb6662a9c95021cf22087663c9681e3135ba63be
Dejan Ardov <dejanardov@gmail.com>
2026-05-16 18:56:19 +0200

    dodavanje metoda executeOneStep i contextSwitch u klasi CPU

--- osSimulacija/src/CPU.java
+++ osSimulacija/src/CPU.java
@@ -7,7 +7,32 @@ public class CPU {
         this.cycleCount = 0;
     }
 
+    public void executeOneStep() {
+        if (current == null) return;
+        current.setProgramCounter(current.getProgramCounter() + 1);
+        cycleCount++;
+        if (current.getProgramCounter() >= current.getBurstTime()) {
+            current.setState(ProcessState.TERMINATED);
+        }
+    }
+
+    public void contextSwitch(PCB next) {
+        if (current != null) {
+            current.setState(ProcessState.READY);
+        }
+        this.current = next;
+        if (next != null) {
+            next.setState(ProcessState.RUNNING);
+        }
+    }
+
     public PCB getCurrent() {
         return current;
     }
+
+
+    public long getCycleCount() {
+        return cycleCount;
+    }
+
 }
