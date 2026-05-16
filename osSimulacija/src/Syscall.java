47f809c0a1317f7a846cbb203555015f5b3024a6
Dejan Ardov <dejanardov@gmail.com>
2026-05-16 19:04:16 +0200

    dodavanje konstruktora i getera u klasi Syscall

--- osSimulacija/src/Syscall.java
+++ osSimulacija/src/Syscall.java
@@ -2,7 +2,15 @@ import java.util.List;
 
 public class Syscall {
     private SyscallType type;
-    private List<String> agrs;
+    private List<String> args;
 
+
+    public Syscall(SyscallType type, List<String> args) {
+        this.type = type;
+        this.args = args;
+    }
+
+    public SyscallType getType() { return type; }
+    public List<String> getArgs() { return args; }
 
 }
