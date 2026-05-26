e326136d3f8c79ffa6f4c0d24e3a6a6d28293fea
MilanD20 <milanderajic1707@gmail.com>
2026-05-26 11:45:45 +0200

    Dodavanje metoda read i write u klasi OpneFileHandle.

--- osSimulacija/src/OpenFileHandle.java
+++ osSimulacija/src/OpenFileHandle.java
@@ -1,6 +1,27 @@
 public class OpenFileHandle {
     private File file;
-    private int position;
-    private FileMode mode;
+    private int cursor;
+
+
+    public OpenFileHandle(File file) {
+        this.file = file;
+        this.cursor = 0;
+    }
+
+    public String read(int size){
+        String content = file.read();
+        int end =Math.min(cursor+size,content.length());
+
+        String data = content.substring(cursor,end);
+        cursor = end;
+        return data;
+
+    }
+
+    public void write(String data){
+        file.append(data);
+        cursor += data.length();
+    }
+
 
 }
