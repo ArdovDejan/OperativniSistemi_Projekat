e747c5f4636ab089bd113a1a031228b0ad29b5cb
MilanD20 <milanderajic1707@gmail.com>
2026-05-30 12:06:44 +0200

    Dodavanje boot() metode u klasu OSKarnel.

--- osSimulacija/src/IOMenager.java
+++ osSimulacija/src/IOMenager.java
@@ -5,6 +5,11 @@ public class IOMenager {
     private List<IODevice> devices;
     private OSKarnel karnel;
 
+    public IOMenager(OSKarnel osKarnel) {
+        this.karnel=osKarnel;
+        this.devices=new ArrayList<>();
+    }
+
 
     public void complitedIO(IODevice device, PCB p) {
         System.out.println("[IOMenager] Uredjaj " + device.getName() + " je zavrsio operaciju za proces " + p.getPid());
--- osSimulacija/src/OSKarnel.java
+++ osSimulacija/src/OSKarnel.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class OSKarnel {
@@ -12,8 +13,36 @@ public class OSKarnel {
     private IOMenager ioMenager;
     private int nextPid;
 
+ public OSKarnel() {
+     this.processTable = new ArrayList<>();
+     this.nextPid=1;
+ }
+
+public void boot(){
+    System.out.println("--- [Sistem] Pokretanje OS Simulatora ---");
+
+    RAM ram=new RAM(1024,new int[1024]);
+    this.memoryManager=new MemoryManager(ram);
+    System.out.println("[Sistem] RAM memorija inicijalizovana (1kb).");
+
+    this.ioMenager=new IOMenager(this);
+
+    DiskDevice disk=new DiskDevice("HardDisk_0",this.ioMenager);
+    System.out.println("[Sistem] Disk uredjaj spreman.");
+
+    this.fileSystem=new FileSystem(disk,512);
+    System.out.println("[Sistem] File sistem montiran.");
+
+    this.readyQueue=new ReadyQueue();
+    this.blockedQueue = new BlockedQueue(new ArrayList<>());
+    this.scheduler=new HRRNScheduler(5);
+
+    this.cpu=new CPU();
+    System.out.println("[Sistem] CPU spreman.");
+    System.out.println("--- [Sistem] Boot proces zavrsen uspjesno. ---");
 
 
 
+}
 
 }
