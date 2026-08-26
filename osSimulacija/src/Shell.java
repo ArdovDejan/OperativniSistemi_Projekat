822d3394b682d2bbc915b01997d5a05090242c85
MilanD20 <milanderajic1707@gmail.com>
2026-08-26 11:27:16 +0200

    Dodavanje komandi u klasi shell.

--- osSimulacija/src/Shell.java
+++ osSimulacija/src/Shell.java
@@ -4,11 +4,13 @@ public class Shell {
     private OSKarnel osKarnel;
     private Scanner scanner;
     private boolean running;
+    private String currentPath;
 
     public Shell(OSKarnel osKarnel) {
         this.osKarnel = osKarnel;
         this.scanner = new Scanner(System.in);
         this.running = true;
+        this.currentPath = "/";
     }
 
     public void start() {
@@ -41,10 +43,87 @@ public class Shell {
             case "kill":
                 commandKill(info);
                 break;
+            case "ls":
+                commandLs();
+                break;
+            case "cd":
+                commandCd(info);
+                break;
+            case "mkdir":
+                commandMkdir(info);
+                break;
+            case "rm":
+                commandRm(info);
+                break;
+            
             default:
                 System.out.println("Nepoznata komanda: " + command);
                 break;
         }
+    }
+
+    private void commandLs(){
+        FSNode node=osKarnel.getFileSystem().resolvePath(currentPath);
+        if(node instanceof Directory){
+            Directory dir=(Directory)node;
+            System.out.println("Sadrzaj direktorijuma [" + currentPath + "]:");
+            for(FSNode child : dir.list()){
+                String tip =(child instanceof Directory) ? "<DIR>" : "<<FILE>";
+                System.out.println(" "+ tip+" "+child.getName());
+
+            }
+        }
+
+    }
+    private void commandCd(String[] info){
+        if(info.length<2 || info[1].equals("/")){
+            currentPath = "/";
+            return;
+        }
+        String target=info[1];
+        String targetPath;
+
+        if(target.equals("..")){
+            if(currentPath.equals("/"))return;
+            int lastSlash=currentPath.lastIndexOf("/");
+            targetPath=currentPath.substring(0,lastSlash);
+            if(targetPath.isEmpty()) targetPath="/";
+
+
+        }else{
+            targetPath=currentPath.equals("/") ? "/" + target : currentPath + "/" +  info[1];
+
+        }
+        FSNode node=osKarnel.getFileSystem().resolvePath(targetPath);
+        if(node instanceof Directory){
+            currentPath=targetPath;
+
+        }else{
+            System.out.println("Greska: Direktorijum ne postoji.");
+        }
+
+
+    }
+
+    private void commandMkdir(String[] info){
+        if(info.length<2){
+            System.out.println("Upotreba: mkdir <naziv_direktorijuma>");
+            return;
+
+        }
+        osKarnel.getFileSystem().createDirectory(currentPath,info[1]);
+        System.out.println("Kreiran direktorijum: " + info[1] );
+
+    }
+    private void commandRm(String[] info){
+        if(info.length<2){
+            System.out.println("Upotreba: rm <naziv_fajla_ili_foldera>");
+            return;
+        }
+        String targetPath=currentPath.equals("/") ? "/" + info[1] : currentPath + "/" +  info[1];
+        osKarnel.getFileSystem().delete(targetPath);
+
+
     }
 
     private void commandRun(String[] dijelovi) {
